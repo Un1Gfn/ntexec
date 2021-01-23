5199c98f3a6fadb0b7d2aa74e6483bb24b48013d
Darren Ng <darren19970810@gmail.com>
2021-01-23 15:26:12 +0800

    auto release

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 function confirm {
+  echo -n "Have you pushed the changes? "
+  read -r
+  echo
   echo -n "Are you sure to delete ALL releases, ALL remote tags and ALL local tags? "
   read -r
   echo
@@ -38,8 +41,8 @@ function delete_all_local_and_remote_tags {
 
   # https://devconnected.com/how-to-delete-local-and-remote-tags-on-git/
   # Delete a remote Git tag
-  # proxychains /usr/bin/git push --delete origin <tag>
-  # proxychains /usr/bin/git push origin :refs/tags/<tag>
+  # proxychains -q /usr/bin/git push --delete origin <tag>
+  # proxychains -q /usr/bin/git push origin :refs/tags/<tag>
   # printf "prefix-%s " a b c d
 
   echo "1. Delete all local tags"
@@ -47,14 +50,14 @@ function delete_all_local_and_remote_tags {
   echo
 
   echo "2. Fetch all remote tags"
-  proxychains /usr/bin/git fetch --tags --verbose
-  # proxychains /usr/bin/git fetch --force --tags --verbose
+  proxychains -q /usr/bin/git fetch --tags --verbose
+  # proxychains -q /usr/bin/git fetch --force --tags --verbose
   echo
 
   echo "3. Delete all remote tags"
   tags="$(git tag --list)"
   if [ -n "$tags" ]; then
-    printf " :refs/tags/%s " "$tags" | xargs proxychains /usr/bin/git push --verbose origin
+    printf " :refs/tags/%s " "$tags" | xargs proxychains -q /usr/bin/git push --verbose origin
   fi
   echo
 
@@ -72,6 +75,10 @@ function tag_and_release {
   # echo "$description"
   git tag "$pkgver"
   # for i in "$@"; do echo "#$i#"; done
+  echo "Creating release ..."
+  echo "Title: \"$pkgver\""
+  echo "Notes: \"$notes\""
+  echo "Assets: $*" # https://github.com/koalaman/shellcheck/wiki/SC2145
   gh release create "$pkgver" "$@" --notes "$notes" --prerelease --title "$pkgver"
 }
 
